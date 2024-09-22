8b1562e82c5ad7fe34b45e047e416edfb156b67c
Sameer Singh <sameersingh1600@gmail.com>
2024-09-22 19:30:09 -0400

    Added controller for teams and updated current routes

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from 'express';
-import cors from 'cors';
+import cors from 'cors'; 
 import playersRoutes from './routes/playersRoutes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors()); 
+app.use(express.json()); 
 
-app.use(express.json());
-
-app.use('/api/players', playersRoutes);
+app.use('/api', playersRoutes); 
 
 app.listen(8080, () => {
     console.log('Server Started on http://localhost:8080');
--- routes/playersRoutes.js
+++ routes/playersRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import { getPlayersData, updatePlayerMetrics } from '../controllers/playersControllers.js';
+import { getPlayersData, getPlayerById, updatePlayerMetrics } from '../controllers/playersControllers.js';
+import { getTeamData } from '../controllers/teamsControllers.js';
 
 const router = express.Router();
 
 router.get('/', getPlayersData);
+router.get('/players', getPlayersData);
+
+router.get('/:id', getPlayerById);
+router.get('/players/:id', getPlayerById);
+
 router.put('/update/:id', updatePlayerMetrics);
+
+router.get('/teams/:id', getTeamData);
 
 export default router;
