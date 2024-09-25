ea85f783523ad1e213e63603017402a12d0f8813
Sameer Singh <sameersingh1600@gmail.com>
2024-09-25 14:28:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import playersRoutes from './routes/playersRoutes.js';
 
 const app = express();
 
-const cors = require('cors');
 app.use(cors()); 
 app.use(express.json()); 
 
@@ -13,6 +12,6 @@ app.use('/api', playersRoutes);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log('Server Started on http://localhost:8080');
+    console.log(`Server started on http://localhost:${PORT}`);
     console.log('Press CTRL + C to stop server');
 });
