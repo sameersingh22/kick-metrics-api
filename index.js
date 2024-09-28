e97661c217b68274e182997ec92744796401a0f1
Sameer Singh <63572454+sameersingh22@users.noreply.github.com>
2024-09-28 01:40:50 -0400

    Update index.js
    Added home route

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); 
 
+app.get('/', (req, res) => {
+    res.send(`
+        Welcome to the Kick Metrics API! 
+
+        Player Endpoints:
+          GET /api/players 
+            Returns a list of all players.
+          
+          GET /api/players/:id 
+            Returns detailed information for a specific player identified by id.
+          
+          PUT /api/update/:id 
+            Updates the goals and assists metrics for a player identified by id.
+
+        Team Endpoints:
+          GET /api/teams/:id 
+            Returns the team information and its associated players for the specified id.
+    `);
+}); 
+
 app.use('/api', playersRoutes); 
 
 const PORT = process.env.PORT || 8080;
